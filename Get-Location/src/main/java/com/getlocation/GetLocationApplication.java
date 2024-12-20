e23ea3a0605a0dd9d94bc40c6e647066ebd4772f
guptabhishek4 <ag444982@gmail.com>
2024-12-20 11:22:27 +0530

    initial commit for kafka implementation

---
+++ Get-Location/src/main/java/com/getlocation/GetLocationApplication.java
@@ -0,0 +1,13 @@
+package com.getlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetLocationApplication.class, args);
+	}
+
+}
---
+++ Get-Location/src/test/java/com/getlocation/GetLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
